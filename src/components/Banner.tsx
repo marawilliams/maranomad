dd0efe7ebd934d52eb5fe02223f27f1d5337d739
marawilliams <mara.williams005@gmail.com>
2026-01-05 00:25:08 -0800

    favicon now working

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
         welcome to <br />
       </h2>
       <h3 className="text-center mb-8 text-6xl font-normal leading-[72px] tracking-[0.9px] max-sm:text-xl max-[400px]:text-lg text-[#d7d7d7]">
-        upcycled and personalized.
+        upcycled and personalized...
       </h3>
       <div className=" font-nightly flex justify-center items-center gap-3 pb-8 max-[400px]:flex-col max-[400px]:gap-1 w-[420px] max-sm:w-[350px] max-[400px]:w-[300px]">
 <Link to="/shop" className="mb-10 bg-[#d7d7d7] text-[#09140d] text-center text-3xl border-[#d7d7d7] border-2  font-normal tracking-[1.5px] w-full h-16 flex items-center justify-center hover:bg-[#9e9f96] hover:text-[#d7d7d7] transition-colors duration-500">
