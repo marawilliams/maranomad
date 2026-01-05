4781050e91806691f4cf2350333dcc8740c3f795
marawilliams <mara.williams005@gmail.com>
2026-01-04 21:15:39 -0800

    images

--- src/components/HomeCollectionSection.tsx
+++ src/components/HomeCollectionSection.tsx
@@ -6,7 +6,7 @@ const HomeCollectionSection = () => {
     <div>
       <div className="max-w-screen-2xl flex items-center justify-between mx-auto mt-24 px-5 max-[400px]:px-3">
         <h2 className="text-black text-5xl font-normal tracking-[1.56px] max-sm:text-4xl">
-          Our Collection
+          works
         </h2>
       </div>
       <ProductGridWrapper limit={6}>
--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,11 +1,9 @@
 import { Link } from "react-router-dom";
-import { formatCategoryName } from "../utils/formatCategoryName";
 
 const ProductItem = ({
   id,
   image,
   title,
-  category,
   price,
   popularity: _popularity,
   stock: _stock,
@@ -32,26 +30,9 @@ const ProductItem = ({
       >
         <h2>{title}</h2>
       </Link>
-      <p className="text-secondaryBrown text-lg tracking-wide text-center max-md:text-base">
-        {formatCategoryName(category)}{" "}
-      </p>
       <p className="text-black text-2xl text-center font-bold max-md:text-xl">
         ${price}
       </p>
-      <div className="w-full flex flex-col gap-1">
-        <Link
-          to={`/product/${id}`}
-          className="text-white bg-secondaryBrown text-center text-xl font-normal tracking-[0.6px] leading-[72px] w-full h-12 flex items-center justify-center max-md:text-base"
-        >
-          View product
-        </Link>
-        <Link
-          to={`/product/${id}`}
-          className="bg-white text-black text-center text-xl border border-[rgba(0, 0, 0, 0.40)] font-normal tracking-[0.6px] leading-[72px] w-full h-12 flex items-center justify-center max-md:text-base"
-        >
-          Learn more
-        </Link>
-      </div>
     </div>
   );
 };
